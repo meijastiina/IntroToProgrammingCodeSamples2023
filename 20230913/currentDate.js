489bbf6759668d5610077d51b12c672150c667fb
“Meija <mlohiniva@hotmail.com>
2023-09-13 09:10:58 +0300

    Added a comment

--- 20230913/currentDate.js
+++ 20230913/currentDate.js
@@ -2,4 +2,5 @@
 let currentDate = new Date();
 // Print out the current date
 process.stdout.write(currentDate.toString());
+// Print out current year
 process.stdout.write("\nYear: " + currentDate.getFullYear() + '\n');
